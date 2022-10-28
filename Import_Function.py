8707b126a08770d3121b50686f1c2d78aa0dd176
firoztct <firoztngbd@gmail.com>
2022-10-28 21:24:50 +0600

    image download

---
+++ Import_Function.py
@@ -0,0 +1,23 @@
+def addition2(a, b):
+    sum = a + b
+    return sum
+
+def mul_addition (* args):
+    result = 0
+    for number in args:
+        result += number
+    return result
+
+def subs (a,b):
+    subst = a - b
+    return subst
+
+def multiply(* args):
+    result = 1
+    for number in args:
+        result *= number
+    return result
+
+def divission(a,b):
+    result = a / b
+    return result
